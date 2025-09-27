b2f7438850da5603e17139c70d7e7f6d7b070930
sudarshan pawar <sudarshanpawar7171@gmail.com>
2025-09-27 07:15:56 +0530

    ..

--- src/loops/LcmPrime.java
+++ src/loops/LcmPrime.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class LcmPrime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the First Number : ");
 		int num1 = sc.nextInt();
