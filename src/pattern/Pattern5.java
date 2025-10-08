bfe5c4cb5544f257621134376333471f28802015
sudarshan pawar <sudarshanpawar7171@gmail.com>
2025-10-08 11:33:38 +0530

    this is todays task

---
+++ src/pattern/Pattern5.java
@@ -0,0 +1,50 @@
+package pattern;
+
+class Pattern5
+{
+	public static void main(String [] args)
+	{
+		int n = 21;
+		for(int i = 0;i<n;i++)
+		{
+			for(int j = 0;j<n;j++)
+			{	
+				if( i==0||j==0 || j == n-1 || i == n-1 || j== n/2 || i == n/2 || i + j == n-1 || i == j)
+				{
+				   System.out.print("*" + " ");
+				}
+				else{
+				    System.out.print(" " + " ");
+				}
+			}
+
+			System.out.println();
+		}
+	}
+}
+
+/*
+output :-
+
+* * * * * * * * * * * * * * * * * * * * *
+* *                 *                 * *
+*   *               *               *   *
+*     *             *             *     *
+*       *           *           *       *
+*         *         *         *         *
+*           *       *       *           *
+*             *     *     *             *
+*               *   *   *               *
+*                 * * *                 *
+* * * * * * * * * * * * * * * * * * * * *
+*                 * * *                 *
+*               *   *   *               *
+*             *     *     *             *
+*           *       *       *           *
+*         *         *         *         *
+*       *           *           *       *
+*     *             *             *     *
+*   *               *               *   *
+* *                 *                 * *
+* * * * * * * * * * * * * * * * * * * * *
+*/
