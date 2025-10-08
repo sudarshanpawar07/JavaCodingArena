0a6a65ffba1dde6ebdda4057fabfd4992323784c
sudarshan pawar <sudarshanpawar7171@gmail.com>
2025-10-08 11:32:03 +0530

    this is todays patterns

---
+++ src/pattern/Pattern4.java
@@ -0,0 +1,37 @@
+package pattern;
+
+class Pattern4
+{
+	public static void main(String [] args)
+	{
+		int n = 7;
+		for(int i = 0;i<n;i++)
+		{
+			for(int j = 0;j<n;j++)
+			{	
+				if( j== n/2 || i == n/2)
+				{
+				   System.out.print("*" + " ");
+				}
+				else{
+				    System.out.print(" " + " ");
+				}
+			}
+
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+output :- 
+
+      *
+      *
+      *
+* * * * * * *
+      *
+      *
+      *
+*/
