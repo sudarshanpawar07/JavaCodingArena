14e1a7f75b211ea5a4d33bd2a961141e81530e50
sudarshan pawar <sudarshanpawar7171@gmail.com>
2025-10-12 07:40:08 +0530

    this is todays practice of patterns

---
+++ src/pattern/Pattern8.java
@@ -0,0 +1,52 @@
+package pattern;
+
+class Pattern8
+{
+	public static void main(String [] args)
+	{
+		int n = 21;
+		for(int i = 0;i<n;i++)
+		{
+			for(int j = 0;j<n;j++)
+			{	
+				if(j == n/2 || i == n/2 || i+j == n-1 || i==j|| i== 0 && j >= n/2 ||i >= n/2 && j==n-1 || j <= n/2 && i == n-1 || j == 0 && i <= n/2)
+				{
+				   System.out.print("*" + " ");
+				}
+				else{
+				    System.out.print(" " + " ");
+				}
+			}
+
+			System.out.println();
+		}
+	}
+}
+
+/*
+ 
+*                   * * * * * * * * * * * 
+* *                 *                 *   
+*   *               *               *     
+*     *             *             *       
+*       *           *           *         
+*         *         *         *           
+*           *       *       *             
+*             *     *     *               
+*               *   *   *                 
+*                 * * *                   
+* * * * * * * * * * * * * * * * * * * * * 
+                  * * *                 * 
+                *   *   *               * 
+              *     *     *             * 
+            *       *       *           * 
+          *         *         *         * 
+        *           *           *       * 
+      *             *             *     * 
+    *               *               *   * 
+  *                 *                 * * 
+* * * * * * * * * * *                   * 
+
+ */
+
+
---
+++ src/pattern/Pattern9.java
@@ -0,0 +1,40 @@
+package pattern;
+
+class Pattern9
+{
+	public static void main(String [] args)
+	{
+		int n = 9;
+		for(int i = 0;i<n;i++)
+		{
+			for(int j = 0;j<n;j++)
+			{	
+				if(i == 0 && j <= n/4 || j == 0 && i <= n/4 || i == 0 && j >= 3*n/4 || j == n-1 && i <= n/4 ||
+				 j == 0 && i >= 3*n/4 ||i == n-1  && j <= n/4 || i == n-1 && j >= 3*n/4|| j == n-1 && i >= 3*n/4 )
+				{
+				   System.out.print("*" + " ");
+				}
+				else{
+				    System.out.print(" " + " ");
+				}
+			}
+
+			System.out.println();
+		}
+	}
+}
+
+/*
+
+* * *       * * * 
+*               * 
+*               * 
+                  
+                  
+                  
+*               * 
+*               * 
+* * *       * * * 
+
+
+*/
