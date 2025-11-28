45b0c022f77926bc9557b0279291ed6a346f5f04
sudarshan pawar <sudarshanpawar7171@gmail.com>
2025-11-28 23:09:14 +0530

    .

---
+++ src/ExceptionHandling/Scenario10.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+public class Scenario10 {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Try");
+			
+		}catch()
+		{
+			
+		}
+	}
+
+}
