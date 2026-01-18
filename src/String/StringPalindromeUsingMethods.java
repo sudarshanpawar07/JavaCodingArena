bb094d788bd46f242684debb1e17aa5ca3e8c617
sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-01-18 20:45:14 +0530

    This is Todays practice

---
+++ src/String/StringPalindromeUsingMethods.java
@@ -0,0 +1,13 @@
+package String;
+
+public class StringPalindromeUsingMethods {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder("NAMAN");
+
+		StringBuilder str2 = str.reverse();
+
+		System.out.println(str == str2);
+
+	}
+
+}
