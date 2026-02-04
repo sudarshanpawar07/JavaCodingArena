e83725e904b492555ae73e40c2831b4dcd5323c9
sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-02-04 21:52:49 +0530

    I have started learning cursor in java enumeration Example

---
+++ src/Collection/EnumerationExample.java
@@ -0,0 +1,24 @@
+package Collection;
+
+import java.util.LinkedList;
+
+public class EnumerationExample {
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for (int i = 1; i <= 15; i++) {
+			list.add((int) (Math.random() * 100));
+		}
+		System.out.println(list);
+
+		for (int i = 0; i < list.size(); i++) {
+			Integer ele = list.get(i);
+			if (ele % 2 != 0) {
+				list.remove(i);
+				i--;
+			}
+
+		}
+		System.out.println(list);
+	}
+
+}
