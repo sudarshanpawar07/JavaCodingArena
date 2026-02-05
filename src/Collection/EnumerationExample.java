168e9a9941d01bcd874ebe8f3410bddc36a99494
.sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-02-05 10:59:50 +0530

    .

--- src/Collection/EnumerationExample.java
+++ src/Collection/EnumerationExample.java
@@ -1,11 +1,10 @@
 package Collection;
-
 import java.util.LinkedList;
 
 public class EnumerationExample {
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<Integer>();
-		for (int i = 1; i <= 15; i++) {
+		for (int i = 1; i <= 5; i++) {
 			list.add((int) (Math.random() * 100));
 		}
 		System.out.println(list);
