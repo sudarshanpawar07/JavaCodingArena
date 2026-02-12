21d265410147837d7e1095484d18119a6066fd90
sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-02-12 23:06:41 +0530

    this is custom class i have created in order to sort

---
+++ src/Collection/Student1.java
@@ -0,0 +1,16 @@
+package Collection;
+
+public class Student1 {
+	String name;
+	int age;
+	double per;
+	String deg;
+
+	Student1(String name, int age, double per, String deg) {
+		this.age = age;
+		this.per = per;
+		this.deg = deg;
+		this.name = name;
+
+	}
+}
---
+++ src/Collection/StudentDriver.java
@@ -0,0 +1,11 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class StudentDriver {
+	public static void main(String[] args) {
+		ArrayList<Student1> al = new ArrayList<Student1>();
+
+	}
+
+}
