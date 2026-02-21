bd6b47a9ac59fc25f7e5e1c6c4bf8e4a55eada98
sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-02-21 15:58:20 +0530

    This is todays practice

---
+++ src/Abstract/Demo.java
@@ -0,0 +1,16 @@
+package Abstract;
+
+public abstract class Demo {
+	static{
+		m1();
+	}
+
+	{
+		
+	}
+	public static void m1()
+	{
+		System.out.println("hello from m1 ");
+	}
+
+}
---
+++ src/set/Test.java
@@ -0,0 +1,46 @@
+package set;
+
+import java.util.Objects;
+
+public class Test {
+	int id;
+	String name;
+	float per;
+	long contact;
+	char gen;
+
+	public Test() {
+		super();
+
+	}
+
+	public Test(int id, String name, float per, long contact, char gen) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.per = per;
+		this.contact = contact;
+		this.gen = gen;
+	}
+
+	@Override
+	public int hashCode() {
+		// return Objects.hash(contact, gen, id, name, per);
+		return Integer.hashCode(id) + name.hashCode() + Float.hashCode(per) + Long.hashCode(contact)
+				+ Character.hashCode(gen);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Test other = (Test) obj;
+		return contact == other.contact && gen == other.gen && id == other.id && Objects.equals(name, other.name)
+				&& Float.floatToIntBits(per) == Float.floatToIntBits(other.per);
+	}
+
+}
