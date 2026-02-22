b3c3e33c97764745f12821fe16f7387c4c499555
sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-02-22 14:50:10 +0530

    I have created LinkedList class and it has partial implementation

---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,8 @@
+package linkedList;
+
+public class LinkedList <E>{
+	int index;
+	Node<E> head;
+	Node<E> tail;
+
+}
---
+++ src/linkedList/Node.java
@@ -0,0 +1,43 @@
+package linkedList;
+
+import java.util.Objects;
+
+public class Node<E> {
+	Node<E> prev;
+	Node<E> next;
+	E val;
+
+	private Node() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Node(Node<E> prev, Node<E> next, E val) {
+		super();
+		this.prev = prev;
+		this.next = next;
+		this.val = val;
+	}
+
+	@Override
+	public String toString() {
+		return "" + val;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(val);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node<E> other = (Node<E>) obj;
+		return Objects.equals(val, other.val);
+	}
+
+}
