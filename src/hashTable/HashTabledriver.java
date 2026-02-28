e64bf2520b99be3dbe5d0ad146b79265aff97ba0
sudarshan pawar <sudarshanpawar7171@gmail.com>
2026-02-28 11:27:44 +0530

    ..

--- src/hashTable/HashTabledriver.java
+++ src/hashTable/HashTabledriver.java
@@ -2,6 +2,13 @@ package hashTable;
 
 public class HashTabledriver {
 	public static void main(String[] args) {
+		HashTable ht = new HashTable();
+		ht.add(10);
+		ht.add(20);
+		ht.add(30);
+		System.out.println(ht);
+		ht.remove(20);
+		System.out.println(ht);
 		
 	}
 
